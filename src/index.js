8fcac14af07bbb352b9b7381090f817068e25ed9
Maiker Hernández <maikeralejandro@hotmail.com>
2025-03-19 13:26:40 -0500

    feat: Añadir eventListener a boton de inicio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css"; //importamos el css en el punto de entrada
+import dibujarPaginaInicio from "./pagina-inicio.js";
+
+const contenedor = document.querySelector("#container");
+const botonInicio = document.querySelector("#boton-inicio");
+
+botonInicio.addEventListener("click", function () {
+  dibujarPaginaInicio(contenedor);
+});
